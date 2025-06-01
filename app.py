09162d53f56869212852183788e7604a8075e7ca
kst2221 <kst2221@naver.com>
2025-06-01 14:58:15 +0900

    app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from threading import Thread
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "✅ I'm alive!", 200
+    return "✅ I'm alive!"
 
-# Render는 기본적으로 0.0.0.0:10000 이상의 포트를 허용함
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
